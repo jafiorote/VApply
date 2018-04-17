bffbd1088f9d5666adbf5a2724933e62bc71af3f
lcirqueira <leonardocp21@gmail.com>
2018-04-17 14:43:40 -0300

    add dir variables; add queue option qsub

--- main.sh
+++ main.sh
@@ -2,10 +2,24 @@ name=$1
 initial=$2
 nodes=$3
 wall=$4
-first=False
 first=$5
+if [ -z "$5" ]
+    then
+        first=False
+fi
+depend=$6
+if [ -z "$6" ]
+    then
+        depend=False
+fi
 cation=XXX
 anion=XXX
 
 ./pbsgen.sh $name $initial $nodes $wall $first $cation $anion> pbsjob.pbs
-/usr/bin/qsub pbsjob.pbs
+
+if [[ $depend == False ]]
+    then
+        /usr/bin/qsub pbsjob.pbs
+    else
+        /usr/bin/qsub pbsjob.pbs -W depend=afterany:$depend
+fi
--- pbsgen.sh
+++ pbsgen.sh
@@ -6,10 +6,16 @@ first=$5
 cation=$6
 anion=$7
 pwddir=`pwd`
+vmddir="PYTHONHOME=/cluster/packages/python/python-3.5.2 LD_LIBRARY_PATH=\$PYTHONHOME/lib /cluster/packages/vmd-1.9.2-minimal-python-3/bin"
+namddir="/cluster/intel/impi/5.0.3.048/intel64/bin/mpirun /cluster/packages/namd_2.10_cpu_mpi"
+#vmddir="/home/lcirqueira/install/tclvmd/bin"
+#namddir="/home/lcirqueira/install/namd2/namd2 +idlepoll +p4"
 
 echo "#"PBS -N $name.$initial
-echo "#"PBS -l nodes=$nodes:ppn=4
-#echo "#"PBS -l nodes=$nodes:ppn=40 CHECK YOUR MACHINE!
+#echo "#"PBS -l nodes=$nodes:ppn=4
+#CHECK YOUR MACHINE!
+echo "#"PBS -l nodes=$nodes:ppn=40
+echo "#"PBS -l pmem=300mb
 echo "#"PBS -l walltime=$wall
 echo "#"PBS -j eo
 echo " "
@@ -18,9 +24,9 @@ echo cd $pwddir
 echo " "
 echo if [ $first == First ]
 echo     then
-echo         "/home/lcirqueira/install/tclvmd/bin/vmd -dispdev text -e makexsc.tcl    -args $name $pwddir \$i  0    0  300  &> makexsc.out"
-echo         "/home/lcirqueira/install/tclvmd/bin/vmd -dispdev text -e getcharges.tcl -args $name $pwddir \$i $cation $anion First &> getcharges.out"
-echo         "/home/lcirqueira/install/tclvmd/bin/vmd -dispdev text -e selections.tcl -args $name $pwddir \$i $cation $anion First &> selections.out"
+echo         "${vmddir}/vmd -dispdev text -e makexsc.tcl    -args $name $pwddir \$i  0    0  300  &> makexsc.out"
+echo         "${vmddir}/vmd -dispdev text -e getcharges.tcl -args $name $pwddir \$i $cation $anion First &> getcharges.out"
+echo         "${vmddir}/vmd -dispdev text -e selections.tcl -args $name $pwddir \$i $cation $anion First &> selections.out"
 echo         "./confgen.sh $name \$i \$((i-1)) $pwddir First &> $name.\$i.conf" 
 echo         "echo '"#" SIMULATION_NUMBER ACCUMULATED_TIME SIMULATION_TIME CRASH/FIRST' &> restart.dat"
 echo         "echo "\$i 0 0 FIRST" &>> restart.dat"
@@ -32,13 +38,13 @@ echo " "
 echo while [ True ]
 echo     do
 echo " "
-echo    "/home/lcirqueira/install/namd2/namd2 +idlepoll +p4 $name.\$i.conf &> $name.\$i.out"
+echo    "${namddir}/namd2 $name.\$i.conf &> $name.\$i.out"
 echo " "
 echo     "if [[ \`grep -c 'FATAL ERROR' $name.\$i.out\` != 0 ]]"
 echo         then
 echo            "if [[ \`grep -c 'PleaseCrash' $name.\$i.out\` != 0 ]]"
 echo                then
-echo                    "/home/lcirqueira/install/tclvmd/bin/vmd -dispdev text -e make_restart.tcl -args $name $pwddir \$i $cation $anion &> make_restart.\$i.out" 
+echo                    "${vmddir}/vmd -dispdev text -e make_restart.tcl -args $name $pwddir \$i $cation $anion &> make_restart.\$i.out" 
 echo                    "./confgen.sh $name \$((i+1)) \$i $pwddir True &> $name.\$((i+1)).conf"
 echo                else
 echo                    "echo FATAL ERROR"
